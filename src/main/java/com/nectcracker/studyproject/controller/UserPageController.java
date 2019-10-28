8358f68179d1c9e880309e4fa490e287280abd91
Sophia <sofiya_shalgueva@mail.ru>
2019-10-28 13:28:55 +0300

    Feat : add ui

--- src/main/java/com/nectcracker/studyproject/controller/UserPageController.java
+++ src/main/java/com/nectcracker/studyproject/controller/UserPageController.java
@@ -1,8 +1,16 @@
 package com.nectcracker.studyproject.controller;
 
+<<<<<<< HEAD
 import com.nectcracker.studyproject.domain.UserWishes;
+=======
+import com.nectcracker.studyproject.domain.UserInfo;
+import com.nectcracker.studyproject.repos.UserInfoRepository;
+import com.nectcracker.studyproject.repos.UserRepository;
+>>>>>>> Feat : add ui
 import com.nectcracker.studyproject.service.UserWishesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,10 +21,23 @@ public class UserPageController {
     @Autowired
     private UserWishesService userWishesService;
 
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/cabinet")
     public String cabinet(Map<String, Object> model) {
+<<<<<<< HEAD
         Iterable<UserWishes> list = userWishesService.getUserWishes();
         model.put("messages", list);
+=======
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserInfo currentUserInfo = userInfoRepository.findByUser(userRepository.findByUsername(auth.getName()));
+        model.put("info", currentUserInfo);
+        userWishesService.showWishes(model);
+>>>>>>> Feat : add ui
         return "cabinet";
     }
 }
