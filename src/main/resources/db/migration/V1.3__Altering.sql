c2ec328b731ab3eba32deb4f796ab859af22df14
chudofishe <48070191+chudofishe@users.noreply.github.com>
2019-12-01 17:33:28 +0300

    Update V1.3__Altering.sql

--- src/main/resources/db/migration/V1.3__Altering.sql
+++ src/main/resources/db/migration/V1.3__Altering.sql
@@ -1,7 +1,7 @@
--- alter table user_info
--- add column photo50 varchar(255)
--- ;
+alter table user_info
+add column photo50 varchar(255)
+;
 --
--- alter table user_wishes
--- add column friend_create_wish boolean not null
--- ;
+alter table user_wishes
+add column friend_create_wish boolean not null
+;
